9f1a09300830db7786134d375f69acd2fc60cfc4
DavidS415 <93840316+DavidS415@users.noreply.github.com>
2022-04-23 12:30:03 -0700

    error handling fixes

--- api/controllers/visaApiController.js
+++ api/controllers/visaApiController.js
@@ -17,6 +17,8 @@ const allVisas = (req, res) => {
   })
 }
 
+//return a specific visa
+
 const findThisVisa = (req, res) => {
   const visaName = req.params.name;
   Visa.findOne({ visa: visaName }, function(err, visaDetails) {
@@ -30,7 +32,24 @@ const findThisVisa = (req, res) => {
   })
 }
 
+const visaInstructions = (req, res) => {
+  res.send('You formatted your request incorrectly, please review instructions at wwww.url.com');
+}
+
+const errorHandler = fn => (req, res, next) => {
+  Promise.resolve(fn(req, res, next))
+    .catch(err => {
+      if (err.name === 'BadRequestError') {
+        res.status(400).send({ msg: err.message });
+      } else {
+        res.status(500).send({ msg: 'Internal Server Error' })
+      }
+    });
+};
+
 module.exports = {
   allVisas,
-  findThisVisa
+  findThisVisa,
+  visaInstructions,
+  errorHandler
 }
--- api/routes/visaApiRoutes.js
+++ api/routes/visaApiRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const { route } = require('express/lib/application');
 const router = express.Router();
 
-const { allVisas, findThisVisa } = require('../controllers/visaApiController');
+const { allVisas, findThisVisa, visaInstructions, errorHandler } = require('../controllers/visaApiController');
 
-router.get('/visas', allVisas);
-router.get('/visas/:name', findThisVisa);
+router.get('/', errorHandler(visaInstructions));
+router.get('/visas', errorHandler(allVisas));
+router.get('/visas/:name', errorHandler(findThisVisa));
 
 module.exports = router;
